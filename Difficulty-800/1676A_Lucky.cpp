3f58e0afbc0c83d8d266958014c40e440de79ed3
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-09-08 23:17:09 +0600

    Add files via upload

---
+++ Difficulty-800/1676A_Lucky.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main(){
+
+    int tc;
+    cin>>tc;
+    while(tc--){
+        string str;
+        cin>>str;
+        int sum1 = 0 , sum2 = 0;
+        for(int i = 0; i<3; i++){
+            sum1 += str[i]-'0';
+        }
+        
+        for(int i = 3; i<6; i++){
+            sum2 += str[i]-'0';
+        }
+        if(sum1 == sum2) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+        
+    }
+
+    return 0;
+}
