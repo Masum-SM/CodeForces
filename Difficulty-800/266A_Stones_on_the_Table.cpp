7066de6aa810f75787248202695a266a956e17f5
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-09-16 11:07:21 +0600

    Add files via upload

---
+++ Difficulty-800/266A_Stones_on_the_Table.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    cin>>n;
+    string str;
+    cin>>str;
+    int count = 0;
+    for(int i = 0 ; i < str.length() ; i++){
+        if(str[i] == str[i+1]){
+            count++;
+        }
+    }
+    cout<<count<<endl;
+
+
+    return 0;
+}
