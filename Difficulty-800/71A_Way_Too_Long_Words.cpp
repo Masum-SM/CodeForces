62d9b6c7ffad3511d683984844eb1c7531db133f
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-09-16 11:52:16 +0600

    Add files via upload

---
+++ Difficulty-800/71A_Way_Too_Long_Words.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+   int n;
+    cin>>n;
+    while(n--)
+    {
+        char s[101];
+        cin>>s;
+        if(strlen(s)>10)
+        {
+            cout<<s[0]<<strlen(s)-2<<s[strlen(s)-1]<<endl;
+        }
+        else
+            cout<<s<<endl;
+    }
+    return 0;
+}
