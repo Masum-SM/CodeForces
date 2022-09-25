54825e1ca43dd11a4ea7c99038c0cb930399ff35
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-09-25 10:40:15 +0600

    Update 1328A_Divisibility_Problem.cpp

---
+++ Difficulty-800/1328A_Divisibility_Problem.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main(){
+    int tc;
+    cin>>tc;
+    while(tc--){
+        int x , y;
+        cin>>x>>y;
+        int count = 0;
+        if(x % y == 0){
+            cout<<count<<endl;
+        }
+        else{
+
+        }
+        
+    }
+
+
+    return 0;
+}
